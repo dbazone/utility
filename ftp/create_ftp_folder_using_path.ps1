d474d414c8317aeb7765aba3e39dd009b4d17704
Vijay Mishra <vijay.mishra@shoregrp.com>
2018-12-23 08:40:33 +0530

    inital commit

---
+++ ftp/create_ftp_folder_using_path.ps1
@@ -0,0 +1,32 @@
+﻿
+
+function CreateDirectoriesFromPath ($DirectoryPath, $ServerPath){
+ 
+	$DirectoryParts = $DirectoryPath.Split("/");
+ 
+	$Position = "";
+	
+	for ($i = 0; $i -lt $DirectoryParts.Length - 1; $i++){
+		
+		try {
+
+            Write-Host $ServerPath+$Position
+
+			$AbsoluteTemporaryPath = New-Object System.Uri($ServerPath+$Position);
+			Write-Host $AbsoluteTemporaryPath 
+
+			$WebRequest = [System.Net.WebRequest]::Create($AbsoluteTemporaryPath);
+			$WebRequest.KeepAlive = $false;
+			#$WebRequest.Credentials = New-Object System.Net.NetworkCredential($Login, $Password);
+			$WebRequest.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory;
+			$WebRequest.GetResponse();
+			
+		} catch [Net.WebException] { 
+			Write-Host $_.Exception.Message; continue;
+		}
+	}
+
+}
+
+
+
